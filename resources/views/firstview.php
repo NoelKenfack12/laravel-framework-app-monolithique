5b880db50af753b04d13d460ae16651795545fee
Noel Kenfack <noel.kenfack@sprint-pay.com>
2021-12-28 15:31:56 +0100

    first commit

---
+++ resources/views/firstview.php
@@ -0,0 +1,10 @@
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>Ma première vue</title>
+</head>
+<body>
+    Hello World !
+</body>
+</html>
---
+++ routes/web.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return ['un', 'deux', 'trois'];
+    return view('welcome');
+});
+Route::get('noel/{n}', function($n){
+    return 'Je suis la page ' . $n . ' !';
+})->where('n', '[1-3]');
+
+Route::get('test', function () {
+    return response('un test', 206)->header('Content-Type', 'text/plain');
+});
+
+Route::get('/firstview', function() {
+    return view('firstview');
+});
+    
